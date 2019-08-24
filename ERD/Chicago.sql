ffd4dd29c6942ca577f29ccf8317ae1316c0205d
mirfakhrulhassan <50125428+mirfakhrulhassan@users.noreply.github.com>
2019-08-24 13:09:56 -0400

    Updated

--- ERD/Chicago.sql
+++ ERD/Chicago.sql
@@ -5,6 +5,7 @@ CREATE TABLE RegisterCrime (id SERIAL NOT NULL, id_crime int4 NOT NULL, id_city 
 ALTER TABLE RegisterCrime ADD CONSTRAINT FKRegisterCr539537 FOREIGN KEY (id_city) REFERENCES City (Id);
 ALTER TABLE RegisterCrime ADD CONSTRAINT FKRegisterCr993854 FOREIGN KEY (id_crime) REFERENCES CrimesType (Id);
 ALTER TABLE Census ADD CONSTRAINT FKCensus735415 FOREIGN KEY (id_city) REFERENCES City (Id);
+insert into city (name,country,state) values ('Chicago','USA','Illinois');
 
 ALTER TABLE RegisterCrime DROP CONSTRAINT FKRegisterCr539537;
 ALTER TABLE RegisterCrime DROP CONSTRAINT FKRegisterCr993854;
@@ -13,10 +14,10 @@ DROP TABLE IF EXISTS Census CASCADE;
 DROP TABLE IF EXISTS City CASCADE;
 DROP TABLE IF EXISTS CrimesType CASCADE;
 DROP TABLE IF EXISTS RegisterCrime CASCADE;
-
+drop table TempRegisterCrime;
 
 CREATE TABLE TempRegisterCrime (
-		id_city int,
+		city varchar(100),
 		type varchar(100),
    		day int4, 		
     	month varchar(10), 
@@ -25,4 +26,24 @@ CREATE TABLE TempRegisterCrime (
      	longitude float4
 		)
 		
-drop table TempRegisterCrime;
+select * from crimestype;
+
+-- # CREATE TABLE RegisterCrime (
+-- #     id SERIAL NOT NULL, 
+-- #     id_crime int4 NOT NULL, 
+-- #     id_city int4 NOT NULL, 
+-- #     day int4, 
+-- #     month varchar(10), 
+-- #     year int4, 
+-- #     latitude float4, 
+-- #     longitude float4, PRIMARY KEY (id));
+-- # ALTER TABLE RegisterCrime ADD CONSTRAINT FKRegisterCr539537 FOREIGN KEY (id_city) REFERENCES City (Id);
+-- # ALTER TABLE RegisterCrime ADD CONSTRAINT FKRegisterCr993854 FOREIGN KEY (id_crime) REFERENCES CrimesType (Id);
+
+insert into city (name,country,state) values ('Chicago','USA','Illinois');
+
+select * from city;
+select * from TempRegisterCrime;
+
+insert into registercrime (id_crime,id_city,day,month,year,latitude,longitude) 
+select crimestype.id, city.id,tempregistercrime.day,tempregistercrime.month,tempregistercrime.year,tempregistercrime.latitude,tempregistercrime.longitude from tempregistercrime inner join crimestype on tempregistercrime.type = crimestype.type inner join city on city.name = tempregistercrime.city;  
